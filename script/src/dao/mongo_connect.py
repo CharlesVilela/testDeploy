faf5f469f127e4a302766e7ff89b1d43a6419f54
CharlesVilela <charlesvilela12@gmail.com>
2024-09-05 10:57:39 -0300

    Enviando o Nono Teste

--- script/src/dao/mongo_connect.py
+++ script/src/dao/mongo_connect.py
@@ -1,9 +1,13 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, encryption
 
 def connected_bd():
     # Substitua com sua URL de conexão MongoDB
     # client = MongoClient("mongodb+srv://charlesvilela:user@cluster0.ryzor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    client = MongoClient("mongodb+srv://charlesvilela:user@cluster0.ryzor.mongodb.net/?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+    # client = MongoClient("mongodb+srv://charlesvilela:user@cluster0.ryzor.mongodb.net/?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+    client = MongoClient(
+        "mongodb+srv://charlesvilela:user@cluster0.ryzor.mongodb.net/",
+        ssl_cert_reqs=encryption.server_api.SSL_CERT_REQUIRED
+    )
 
     # Escolha o banco de dados
     db = client["chatbot_chronoschat"]
